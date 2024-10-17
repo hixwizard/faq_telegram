440fdc70659d123dd4ce5ed4fde023ff07962685
hix9 <stanislavbarinov0@gmail.com>
2024-10-17 23:00:39 +0300

    edit models

--- flask_admin.py
+++ flask_admin.py
@@ -7,7 +7,7 @@ from flask_admin.contrib.sqla import ModelView
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import User, Application
+from models import User, UserForm, UserFormStatus
 
 # --- Настройки ---
 app = Flask(__name__)
@@ -26,7 +26,8 @@ admin = Admin(app, name='Admin Zone', template_mode='bootstrap3')
 
 # Добавляем модели в админ-панель
 admin.add_view(ModelView(User, session))
-admin.add_view(ModelView(Application, session))
+admin.add_view(ModelView(UserForm, session))
+admin.add_view(ModelView(UserFormStatus, session))
 
 # Запуск Flask-приложения
 if __name__ == '__main__':
